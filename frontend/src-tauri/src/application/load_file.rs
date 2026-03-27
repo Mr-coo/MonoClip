ff574c925ac74629a77f38f70220a3c46aa9d85b
unknown <marcolinardi00@gmail.com>
2026-03-27 09:41:12 +0700

    refactor: remove previous rust architecture

--- frontend/src-tauri/src/application/load_file.rs
+++
@@ -1,6 +0,0 @@
-use crate::domain::file_entity::FileData;
-use crate::infrastructure::file_repo;
-
-pub fn execute(path: String) -> Result<FileData, String> {
-    file_repo::load_file_metadata(&path)
-}
--- frontend/src-tauri/src/domain/file_entity.rs
+++
@@ -1,8 +0,0 @@
-use serde::Serialize;
-
-#[derive(Serialize)]
-pub struct FileData{
-  pub name: String,
-  pub extension: Option<String>,
-  pub size: u64,
-}
